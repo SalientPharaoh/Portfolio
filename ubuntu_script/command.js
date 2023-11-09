42797d9f2766e27cd67569a924aded08002d18e6
sahil khirwal <sahilkhirwal1012@gmail.com>
2023-11-09 15:16:20 +0530

    adding functions

---
+++ ubuntu_script/command.js
@@ -0,0 +1,145 @@
+var twitter = "https://twitter.com/KhirwalSahil";
+var linkedin = "https://www.linkedin.com/in/sahil-khirwal-464466216/";
+var instagram = "https://www.instagram.com/the_sahil_khirwal/";
+var github = "https://github.com/SalientPharaoh";
+var email = 'mailto:khirwalsahil@gmail.com';
+var clipsurf = "https://github.com/SalientPharaoh/CipSurf_Email_Collection";
+var clipsurfgit="https://clip-surf.vercel.app/";
+var notengit="https://notenavigator.onrender.com/";
+var noten = "https://github.com/SalientPharaoh/NoteNavigator";
+var ebook = "https://github.com/SalientPharaoh/Expense_Book";
+var webscrap = "https://github.com/SalientPharaoh/Scrapper_Continuo";
+
+
+
+whois = [
+  "<br>",
+  "Hey, I'm Sahil",
+  "I'm a passionate computer science and engineering student from Bengaluru, India.", 
+  "with a keen interest in exploring the world of technology. Actively seeking internships,",
+  "I'm driven to become a successful software developer.",
+  "<br>",
+  "Beyond coding, I find joy in immersing",
+  "myself in the worlds of literature and music, playing chess, badminton, guitar, violin, and ",
+  "piano. I can delve into discussions on a wide range of topics, from science and technology to ","international relations, history, geography, finance, and more. With a hunger for knowledge, I'm ",
+  "excited to see where this ever-evolving tech landscape will take us.",
+  "<br>"
+];
+
+
+whoami = [
+  "<br>",
+  "Who knows who we are? This world is a constant search to find out who we are and why we are!",
+  "Keep it a Secret!",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/Sahil_Khirwal' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/Salient_Pharaoh' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/Sahil_Khirwal' + '</a>',
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Sahil_Khirwal' + '</a>',
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Projects are an awesome way to practice and learn.",
+  "Here are some of my projects that i enjoyed building :",
+  "<br>",
+  "<br>",
+  'ClipSurf :      <a href="' + clipsurf + '" target="_blank">website/clipsurf' + "</a>"+ '     |  <a href="' + clipsurfgit + '" target="_blank">github/clipsurf' + "</a>",
+  'Note Navigator :    <a href="' + noten + '" target="_blank">website/notenavigator' + "</a>"+ '    |   <a href="' + notengit + '" target="_blank">github/notenavigator' + "</a>",
+  'Expense Book :     <a href="' + ebook + '" target="_blank">website/expensebook' + "</a>",
+  'Web Scraper :       <a href="' + webscrap + '" target="_blank">website/webscraper' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Sahil?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">resume</span>         View the resume ',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Wish to Email me?',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+    '<span class="inedx">Welcome to Debian 20.04 LTS (GNU/Linux 5.4.0-26-generic x86_64)</span>',
+  "░█▀▀░█▀█░█░█░▀█▀░█░░░░░█░█░█░█░▀█▀░█▀▄░█░█░█▀█░█░░",
+  "░▀▀█░█▀█░█▀█░░█░░█░░░░░█▀▄░█▀█░░█░░█▀▄░█▄█░█▀█░█░░",
+  "░▀▀▀░▀░▀░▀░▀░▀▀▀░▀▀▀░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀",
+  "____________________________________________________",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+resume = [
+"<br>",
+"░█▀▀░█▀█░█░█░▀█▀░█░░░░░█░█░█░█░▀█▀░█▀▄░█░█░█▀█░█░░",
+"░▀▀█░█▀█░█▀█░░█░░█░░░░░█▀▄░█▀█░░█░░█▀▄░█▄█░█▀█░█░░",
+"░▀▀▀░▀░▀░▀░▀░▀▀▀░▀▀▀░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀",
+"________________________________________________________",
+"<br>",
+"╔═╗╔╦╗╦ ╦╔═╗╔═╗╔╦╗╦╔═╗╔╗╔",
+"║╣  ║║║ ║║  ╠═╣ ║ ║║ ║║║║",
+"╚═╝═╩╝╚═╝╚═╝╩ ╩ ╩ ╩╚═╝╝╚╝",
+"Bachelor of Engineering (B.E.) in Computer Science and Engineering",
+"Ramaiah Institute of Technology, Bengaluru",
+"2021-2025",
+"Current CGPA : 9.81",
+"________________________________________________________",
+"<br>",
+"╔═╗═╗ ╦╔═╗╔═╗╦═╗╦╔═╗╔╗╔╔═╗╔═╗",
+"║╣ ╔╩╦╝╠═╝║╣ ╠╦╝║║╣ ║║║║  ║╣ ",
+"╚═╝╩ ╚═╩  ╚═╝╩╚═╩╚═╝╝╚╝╚═╝╚═╝",
+"<br>",
+"Samsung R&D Institute : Research and Development Intern",
+"Project Details : Analyzed the performance of Rust in",
+"building high-performance gRPC-based speech recognition",
+"service,leveraging Rust's concurrency and memory safety ",
+"to optimize response times and resource utilization.",
+"<br>",
+"Ramaiah Institute of Dental Sciences : Backend Developer",
+"Project Details : Played a pivotal role in annotating",
+"real-time images with critical carcinogenic features, ",
+"enhancing the accuracy of the machine learning model and ",
+"contributed to achieving an impressive 85% detection ",
+"accuracy, a 15% improvement over previous iterations.",
+"________________________________________________________",
+"<br>",
+"╔═╗╦═╗╔═╗ ╦╔═╗╔═╗╔╦╗╔═╗",
+"╠═╝╠╦╝║ ║ ║║╣ ║   ║ ╚═╗",
+"╩  ╩╚═╚═╝╚╝╚═╝╚═╝ ╩ ╚═╝",
+"<br>",
+'ClipSurf :      <a href="' + clipsurf + '" target="_blank">website/clipsurf' + "</a>"+ '     |  <a href="' + clipsurfgit + '" target="_blank">github/clipsurf' + "</a>",
+'Note Navigator :    <a href="' + noten + '" target="_blank">website/notenavigator' + "</a>"+ '    |   <a href="' + notengit + '" target="_blank">github/notenavigator' + "</a>",
+'Expense Book :     <a href="' + ebook + '" target="_blank">website/expensebook' + "</a>",
+'Web Scraper :       <a href="' + webscrap + '" target="_blank">website/webscraper' + "</a>",
+"________________________________________________________",
+"<br>",
+"╔═╗╦╔═╦╦  ╦  ╔═╗",
+"╚═╗╠╩╗║║  ║  ╚═╗",
+"╚═╝╩ ╩╩╩═╝╩═╝╚═╝",
+"<br>",
+"Programming Languages: C/C++, Java, Python, Rust",
+"Database Management Systems: Relational Databases ",
+"(SQL, MySQL), Non-Relational (MongoDB), Firebase",
+"Frameworks and Tools: Flask, Django, Git, GitHub Workflows",
+"Others: Web Sockets, REST API, HTML, CSS, UNIX/LINUX",
+"5-star at Hackerrank, 1414 max rating at CodeChef",
+"<br>",
+"core member of official club FOSS club of MSRIT.",
+"Handled a team of 300+ members responsible for conducting ",
+"events for cultivating open-source culture of the college.",
+"________________________________________________________",
+"<br>",
+];
